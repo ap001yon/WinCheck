e7c126e5d032bab35477cdf9f3e8caae1f86fc7f
ap001yon <ap001yon00@gmail.com>
2024-04-20 20:39:54 +0800

    main:1

---
+++ ChildForms/User-Group/UserTest.ps1
@@ -0,0 +1,46 @@
+﻿#Requires -Version 5
+<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2024 v5.8.237
+	 Created on:   	2024/4/5 20:13
+	 Created by:   	Asuka
+	 Organization: 	
+	 Filename:     	UserTest.ps1
+	===========================================================================
+	.DESCRIPTION
+		Description of the PowerShell class.
+#>
+
+
+class UserTest:System.Windows.Forms.Form  {
+	
+	# Class Properties
+	[string]$MyProperty
+	
+	#Hidden Properties
+	hidden [int]$MyHiddenProperty
+	
+	# Class Constructors
+	UserTest::[System.Windows.Forms.Form] ()
+	
+	#Static Class Methods
+	static [Boolean] MyStaticMethod ([int]$parameter)
+	{
+		if ($parameter -gt 0)
+		{
+			return $true
+		}
+		else
+		{
+			return $false
+		}
+	}
+}
+
+#Sample code to instantiate the class
+#$myClassObject = [UserTest:System.Windows.Forms.Form]::new()
+
+#Invoke a static method
+#[UserTest:System.Windows.Forms.Form]::MyStaticMethod(1)
+
---
+++ Globals.ps1
@@ -0,0 +1,235 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+
+#Sample function that provides the location of the script
+function Get-ScriptDirectory
+{
+<#
+	.SYNOPSIS
+		Get-ScriptDirectory returns the proper location of the script.
+
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		Returns the correct path within a packaged executable.
+#>
+	[OutputType([string])]
+	param ()
+	if ($null -ne $hostinvocation)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+
+###返回用户或组的SID
+function GetSidByName
+{
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[String]
+		$name
+	)
+	try
+	{
+		$i = Get-LocalGroup -Name $name -ErrorAction Stop
+		return $i.SID
+	}
+	catch
+	{
+		$i = Get-LocalUser -Name $name 
+		return $i.SID
+	}
+	
+}
+###
+
+###更新ListView中的数据
+function UpdateListView
+{
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[System.Windows.Forms.ListView]$listView,
+		[parameter(Mandatory = $true)]
+		[AllowEmptyCollection()]
+		[System.Collections.ArrayList]$dataList
+	)
+	$listView.Items.Clear()
+	if ($dataList.Count -eq 0)
+	{
+		$saveBtn.Enabled = $false
+	}
+	else
+	{
+		$saveBtn.Enabled = $true
+		$listView.Items.AddRange($dataList)
+	}
+}
+###
+
+### 打开用户添加表，并将所选用户传入datalist
+function OpenAccountForm
+{
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[System.Windows.Forms.ListView]$listView,
+		[parameter(Mandatory = $true)]
+		[AllowEmptyCollection()]
+		[System.Collections.ArrayList]$dataList
+	)
+	if ((Show-AccountForm_psf) -eq 'OK')
+	{
+		foreach ($item in $AccountForm_acListView)
+		{
+			if (!($dataList -contains $item.Text))
+			{
+				$dataList.add($item.Text)
+			}
+		}
+	}
+#	UpdateList -listView $shutDownListView
+}
+###
+### 获取配置文件所需的SID字符串
+function GetCfgSIDs  {
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[Array]
+		$userList
+	)
+	$settingStr = ""
+	$newArray = @()
+	foreach ($u in $userList) {
+		$newArray +="*" +(GetSidByName($u))
+	}
+	[string]$settingStr = $newArray -join ","
+	return $settingStr
+}
+###
+
+###应用设置
+function SetConfig{
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[String]
+		$cfgType
+	)
+	secedit /configure /db 'C:\Windows\security\local.sdb' /cfg "${cfgPath}\${cfgType}.cfg"  /quiet
+}
+
+function DelAcItem  {
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[System.Windows.Forms.ListView]$listView,
+		[parameter(Mandatory = $true)]
+		[AllowEmptyCollection()]
+		[System.Collections.ArrayList]$dataList
+	)
+	foreach ($item in $listView.SelectedItems)
+	{
+		$dataList.Remove($item.Text)
+	}
+}
+
+function WriteCfgFile
+{
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[string]$SIDs,
+		[parameter(Mandatory = $true)]
+		[string]$cfgType
+	)
+	$cfg =
+	@"
+[Unicode]
+Unicode=yes
+[Privilege Rights]
+$cfgType = $SIDs
+[Version]
+signature="`$CHICAGO`$"
+Revision=1
+"@
+	$cfg | Set-Content -Path "${cfgPath}\${cfgType}.cfg"
+	
+}
+
+###生成当前配置文件
+function GetCurrentCfg
+{
+	if (-not (Test-Path -Path $cfgPath -PathType Container))
+	{
+		New-Item -Path $cfgPath -ItemType Directory -Force
+	}
+	secedit /export /cfg $cfgPath\current.cfg
+}
+###
+
+### 获取指配置设置值
+function GetCurrentPolicySetting
+{
+	param
+	(
+		[parameter(Mandatory = $true)]
+		[string]$settingName,
+		[Parameter(Mandatory = $true)]
+		[string]$configStr	
+	)
+	
+	$matchPattern = "(?s)$settingName =[\s]*(\d+)"
+	try
+	{
+		$configStr -match $matchPattern | Out-Null # 匹配成功 
+		$settingValue = $matches[1] # 第一个捕获组（值）
+	}
+	catch
+	{
+		# 匹配失败
+		$settingValue=$null
+	}
+	return $settingValue
+}
+###
+
+function InitPolicySetting
+{
+	# 文件路径
+	$filePath = "${cfgPath}\current.cfg"
+	$configStr = [string](Get-Content -Path $filePath)
+	$policySetting['AuditSystemEvents'] =GetCurrentPolicySetting -settingName 'AuditSystemEvents' -configStr $configStr 
+	$policySetting['AuditLogonEvents'] = GetCurrentPolicySetting -settingName 'AuditLogonEvents' -configStr $configStr
+	$policySetting['AuditObjectAccess'] = GetCurrentPolicySetting -settingName 'AuditObjectAccess' -configStr $configStr
+	$policySetting['AuditPrivilegeUse'] = GetCurrentPolicySetting -settingName 'AuditPrivilegeUse' -configStr $configStr 
+	$policySetting['AuditPolicyChange'] = GetCurrentPolicySetting -settingName 'AuditPolicyChange' -configStr $configStr
+	$policySetting['AuditAccountManage'] = GetCurrentPolicySetting -settingName 'AuditAccountManage' -configStr $configStr
+	$policySetting['AuditProcessTracking'] = GetCurrentPolicySetting -settingName 'AuditProcessTracking' -configStr $configStr
+	$policySetting['AuditDSAccess'] = GetCurrentPolicySetting -settingName 'AuditDSAccess' -configStr $configStr
+	$policySetting['AuditAccountLogon'] = GetCurrentPolicySetting -settingName 'AuditAccountLogon' -configStr $configStr
+	$policySetting['PasswordComplexity'] = GetCurrentPolicySetting -settingName 'PasswordComplexity' -configStr $configStr
+	$policySetting['LockoutBadCount'] = GetCurrentPolicySetting -settingName 'LockoutBadCount' -configStr $configStr
+	$policySetting['LockoutDuration'] = GetCurrentPolicySetting -settingName 'LockoutDuration' -configStr $configStr
+	$policySetting['AllowAdministratorLockout'] = GetCurrentPolicySetting -settingName 'AllowAdministratorLockout' -configStr $configStr
+	$policySetting['MinimumPasswordLength'] = GetCurrentPolicySetting -settingName 'MinimumPasswordLength' -configStr $configStr
+}
+
+
+
+[Collections.Generic.Dictionary[string,string]]$policySetting = @{ }
+[string]$ScriptDirectory = Get-ScriptDirectory
+$acList = @()
+$eventLogProp = @{ }
+$cfgPath = "C:\Users\${env:USERNAME}\AppData\Local\WinCheck"
+
+
